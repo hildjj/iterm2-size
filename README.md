c26780d3ddb8fa59a51f0988bf83e1a2832a45e8
Joe Hildebrand <joe-github@cursive.net>
2025-03-12 13:59:23 -0600

    Initial checkin

---
+++ README.md
@@ -0,0 +1,22 @@
+# iterm2-size
+
+Description
+
+## Installation
+
+```sh
+npm install iterm2-size
+```
+
+## API
+
+Full [API documentation](http://hildjj.github.io/iterm2-size/) is available.
+
+Example:
+
+```js
+```
+
+---
+[![Build Status](https://github.com/hildjj/iterm2-size/workflows/Tests/badge.svg)](https://github.com/hildjj/iterm2-size/actions?query=workflow%3ATests)
+[![codecov](https://codecov.io/gh/hildjj/iterm2-size/branch/main/graph/badge.svg?token=N7B7YLIDM4)](https://codecov.io/gh/hildjj/iterm2-size)
---
+++ typedoc.config.cjs
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('typedoc').TypeDocOptions} */
+module.exports = {
+  entryPoints: ['src/index.ts'],
+  out: 'docs',
+  cleanOutputDir: true,
+  sidebarLinks: {
+    GitHub: 'https://github.com/hildjj/hildjj/iterm2-size/',
+    Documentation: 'http://hildjj.github.io/iterm2-size/',
+  },
+  navigation: {
+    includeCategories: false,
+    includeGroups: false,
+  },
+  includeVersion: true,
+  categorizeByGroup: false,
+  sort: ['static-first', 'alphabetical'],
+  exclude: ['**/*.spec.ts'],
+};
